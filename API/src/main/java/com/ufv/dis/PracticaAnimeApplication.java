980f278d83764514172c1763f643847e4aa9db19
Iker Rodríguez <9005659@alumnos.ufv.es>
2023-01-19 12:52:30 +0100

    Creada la estructura de carpetas inicial

---
+++ API/src/main/java/com/ufv/dis/PracticaAnimeApplication.java
@@ -0,0 +1,13 @@
+package com.ufv.dis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaAnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaAnimeApplication.class, args);
+	}
+
+}
