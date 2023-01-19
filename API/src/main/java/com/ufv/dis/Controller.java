972ff0d16448bc18984d462cbbcd674c1265c48b
Iker Rodríguez <9005659@alumnos.ufv.es>
2023-01-19 13:43:10 +0100

    Implementados métodos Get

--- API/src/main/java/com/ufv/dis/Controller.java
+++ API/src/main/java/com/ufv/dis/Controller.java
@@ -7,5 +7,23 @@ import java.util.List;
 
 @RestController
 public class Controller {
-    
+    List<Anime> listaAnime = new ArrayList<>();
+
+    public void setListaAnime() throws Exception {
+        listaAnime = new LectorJSON().leerJson();
+    }
+
+    @GetMapping("/animes")
+    public List<Anime> getAnimeList() throws Exception {
+        if(listaAnime.size() == 0)
+            setListaAnime();
+        return listaAnime;
+    }
+
+    @GetMapping("/animes/{id}")
+    public Anime getAnime(@PathVariable int id) throws Exception {
+        if(listaAnime.size() == 0)
+            setListaAnime();
+        return listaAnime.get(id);
+    }
 }
